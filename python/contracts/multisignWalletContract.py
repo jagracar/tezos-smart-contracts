839e782ac159fe9931917481e6e868e35ad8fb58
jagracar <jagracar@gmail.com>
2021-11-29 17:36:37 +0100

    Adds a multisign wallet contract

---
+++ python/contracts/multisignWalletContract.py
@@ -0,0 +1,329 @@
+import smartpy as sp
+
+
+class MultisignWalletContract(sp.Contract):
+    """This contract implements a basic multisign wallet.
+
+    Users of the wallet can add their own proposals and vote proposals added by
+    other users. The proposals can be executed when the number of minimum
+    positive votes is reached.
+
+    The contract implements the following types of proposals:
+
+        - Transfer tez from the wallet to another account.
+        - Transfer FA2 tokens from the wallet to another account.
+        - Change the minimum votes parameter.
+        - Add a new user to the wallet.
+        - Remove one user from the wallet.
+
+    TBD:
+        Add a proposal to execute another contract entrypoint. This could be
+        used to collect an OBJKT using the collect entrypoint from the H=N
+        marketplace contract. The required tez would be taken from the wallet
+        and the OBJKT would be transferred to the multisign wallet contract.
+
+    """
+
+    def __init__(self, users, minimum_votes):
+        """Initializes the contract.
+
+        """
+        # Define the contract storage data types for clarity
+        self.init_type(sp.TRecord(
+            users=sp.TSet(sp.TAddress),
+            proposals=sp.TBigMap(sp.TNat, sp.TRecord(
+                type=sp.TString,
+                issuer=sp.TAddress,
+                tez_amount=sp.TOption(sp.TMutez),
+                token_contract=sp.TOption(sp.TAddress),
+                token_id=sp.TOption(sp.TNat),
+                token_amount=sp.TOption(sp.TNat),
+                destination=sp.TOption(sp.TAddress),
+                minimum_votes=sp.TOption(sp.TNat),
+                user=sp.TOption(sp.TAddress),
+                executed=sp.TBool)),
+            votes=sp.TBigMap(sp.TPair(sp.TNat, sp.TAddress), sp.TBool),
+            minimum_votes=sp.TNat,
+            counter=sp.TNat))
+
+        # Initialize the contract storage
+        self.init(
+            users=sp.set(users),
+            proposals=sp.big_map(),
+            votes=sp.big_map(),
+            minimum_votes=minimum_votes,
+            counter=0)
+
+    def check_is_user(self):
+        """Checks that the address that called the entry point is one of the
+        wallet users.
+
+        """
+        sp.verify(self.data.users.contains(sp.sender),
+                  message="This can only be executed by one of the wallet users.")
+
+    @sp.entry_point
+    def transfer_tez_proposal(self, params):
+        """Adds a new transfer tez proposal to the proposals big map.
+
+        """
+        # Define the input parameter data type
+        sp.set_type(params, sp.TRecord(
+            tez_amount=sp.TMutez,
+            destination=sp.TAddress))
+
+        # Check that one of the users executed the entry point
+        self.check_is_user()
+
+        # Update the proposals bigmap with the new proposal information
+        self.data.proposals[self.data.counter] = sp.record(
+            type="transfer_tez",
+            issuer=sp.sender,
+            tez_amount=sp.some(params.tez_amount),
+            token_contract=sp.none,
+            token_id=sp.none,
+            token_amount=sp.none,
+            destination=sp.some(params.destination),
+            minimum_votes=sp.none,
+            user=sp.none,
+            executed=False)
+
+        # Increase the proposals counter
+        self.data.counter += 1
+
+    @sp.entry_point
+    def transfer_token_proposal(self, params):
+        """Adds a new transfer token proposal to the proposals big map.
+
+        """
+        # Define the input parameter data type
+        sp.set_type(params, sp.TRecord(
+            token_contract=sp.TAddress,
+            token_id=sp.TNat,
+            token_amount=sp.TNat,
+            destination=sp.TAddress))
+
+        # Check that one of the users executed the entry point
+        self.check_is_user()
+
+        # Update the proposals bigmap with the new proposal information
+        self.data.proposals[self.data.counter] = sp.record(
+            type="transfer_token",
+            issuer=sp.sender,
+            tez_amount=sp.none,
+            token_contract=sp.some(params.token_contract),
+            token_id=sp.some(params.token_id),
+            token_amount=sp.some(params.token_amount),
+            destination=sp.some(params.destination),
+            minimum_votes=sp.none,
+            user=sp.none,
+            executed=False)
+
+        # Increase the proposals counter
+        self.data.counter += 1
+
+    @sp.entry_point
+    def minimum_votes_proposal(self, params):
+        """Adds a new minimum votes proposal to the proposals big map.
+
+        """
+        # Define the input parameter data type
+        sp.set_type(params, sp.TNat)
+
+        # Check that one of the users executed the entry point
+        self.check_is_user()
+
+        # Check that the proposed minimum votes makes sense
+        sp.verify(params > 0,
+                  message="The minimum_votes parameter should be higher than 0.")
+        sp.verify(params <= sp.len(self.data.users.elements()),
+                  message="The minimum_votes parameter cannot be higher than the number of users.")
+
+        # Update the proposals bigmap with the new proposal information
+        self.data.proposals[self.data.counter] = sp.record(
+            type="minimum_votes",
+            issuer=sp.sender,
+            tez_amount=sp.none,
+            token_contract=sp.none,
+            token_id=sp.none,
+            token_amount=sp.none,
+            destination=sp.none,
+            minimum_votes=sp.some(params),
+            user=sp.none,
+            executed=False)
+
+        # Increase the proposals counter
+        self.data.counter += 1
+
+    @sp.entry_point
+    def add_user_proposal(self, params):
+        """Adds a new add user proposal to the proposals big map.
+
+        """
+        # Define the input parameter data type
+        sp.set_type(params, sp.TAddress)
+
+        # Check that one of the users executed the entry point
+        self.check_is_user()
+
+        # Check that the new user is not in the users list
+        sp.verify(~self.data.users.contains(params),
+                  message="The proposed address is in the users list.")
+
+        # Update the proposals bigmap with the new proposal information
+        self.data.proposals[self.data.counter] = sp.record(
+            type="add_user",
+            issuer=sp.sender,
+            tez_amount=sp.none,
+            token_contract=sp.none,
+            token_id=sp.none,
+            token_amount=sp.none,
+            destination=sp.none,
+            minimum_votes=sp.none,
+            user=sp.some(params),
+            executed=False)
+
+        # Increase the proposals counter
+        self.data.counter += 1
+
+    @sp.entry_point
+    def remove_user_proposal(self, params):
+        """Adds a new remove user proposal to the proposals big map.
+
+        """
+        # Define the input parameter data type
+        sp.set_type(params, sp.TAddress)
+
+        # Check that one of the users executed the entry point
+        self.check_is_user()
+
+        # Check that the user to remove is in the users list
+        sp.verify(self.data.users.contains(params),
+                  message="The proposed address is not in the users list.")
+
+        # Update the proposals bigmap with the new proposal information
+        self.data.proposals[self.data.counter] = sp.record(
+            type="remove_user",
+            issuer=sp.sender,
+            tez_amount=sp.none,
+            token_contract=sp.none,
+            token_id=sp.none,
+            token_amount=sp.none,
+            destination=sp.none,
+            minimum_votes=sp.none,
+            user=sp.some(params),
+            executed=False)
+
+        # Increase the proposals counter
+        self.data.counter += 1
+
+    @sp.entry_point
+    def vote_proposal(self, params):
+        """Adds one vote for a given proposal.
+
+        """
+        # Define the input parameter data type
+        sp.set_type(params, sp.TRecord(
+            proposal_id=sp.TNat,
+            approval=sp.TBool))
+
+        # Check that one of the users executed the entry point
+        self.check_is_user()
+
+        # Check that the proposal id is present in the proposals big map
+        sp.verify(self.data.proposals.contains(params.proposal_id),
+                  message="The provided proposal id doesn't exist.")
+
+        # Check that the proposal has not been executed
+        sp.verify(~self.data.proposals[params.proposal_id].executed,
+                  message="The provided proposal has been executed.")
+
+        # Add or update the users vote
+        self.data.votes[(params.proposal_id, sp.sender)] = params.approval
+
+    @sp.entry_point
+    def execute_proposal(self, params):
+        """Executes a given proposal.
+
+        """
+        # Define the input parameter data type
+        sp.set_type(params, sp.TNat)
+
+        # Check that one of the users executed the entry point
+        self.check_is_user()
+
+        # Check that the proposal id is present in the proposals big map
+        sp.verify(self.data.proposals.contains(params),
+                  message="The provided proposal id doesn't exist.")
+
+        # Check that the proposal has not been executed
+        proposal = self.data.proposals[params]
+
+        sp.verify(~proposal.executed,
+                  message="The provided proposal has already been executed.")
+
+        # Count the proposal total number of positive votes
+        totalVotes = sp.local("totalVotes", 0, sp.TNat)
+
+        sp.for user in self.data.users.elements():
+            sp.if self.data.votes.get((params, user), default_value=False):
+                totalVotes.value += 1
+
+        # Check that the proposal received enough positive votes
+        sp.verify(totalVotes.value >= self.data.minimum_votes,
+                  message="The proposal didn't receive enough positive votes to be executed.")
+
+        # Execute the proposal
+        sp.if proposal.type == "transfer_tez":
+            sp.send(proposal.destination.open_some(),
+                    proposal.tez_amount.open_some())
+
+        sp.if proposal.type == "transfer_token":
+            self.fa2_transfer(
+                fa2=proposal.token_contract.open_some(),
+                from_=sp.self_address,
+                to_=proposal.destination.open_some(),
+                token_id=proposal.token_id.open_some(),
+                token_amount=proposal.token_amount.open_some())
+
+        sp.if proposal.type == "minimum_votes":
+            self.data.minimum_votes = proposal.minimum_votes.open_some()
+
+        sp.if proposal.type == "add_user":
+            self.data.users.add(proposal.user.open_some())
+
+        sp.if proposal.type == "remove_user":
+            self.data.users.remove(proposal.user.open_some())
+
+            # Update the minimum votes parameter if necessary
+            sp.if self.data.minimum_votes > sp.len(self.data.users.elements()):
+                self.data.minimum_votes = sp.as_nat(self.data.minimum_votes - 1)
+
+        # Set the proposal as executed
+        proposal.executed = True
+
+    def fa2_transfer(self, fa2, from_, to_, token_id, token_amount):
+        """Transfers a number of editions of a FA2 token between to addresses.
+
+        """
+        # Get a handle to the FA2 token transfer entry point
+        c = sp.contract(
+            t=sp.TList(sp.TRecord(
+                from_=sp.TAddress,
+                txs=sp.TList(sp.TRecord(
+                    to_=sp.TAddress,
+                    token_id=sp.TNat,
+                    amount=sp.TNat).layout(("to_", ("token_id", "amount")))))),
+            address=fa2,
+            entry_point="transfer").open_some()
+
+        # Transfer the FA2 token editions to the new address
+        sp.transfer(
+            arg=sp.list([sp.record(
+                from_=from_,
+                txs=sp.list([sp.record(
+                    to_=to_,
+                    token_id=token_id,
+                    amount=token_amount)]))]),
+            amount=sp.mutez(0),
+            destination=c)
---
+++ python/tests/multisignWalletContract_test.py
@@ -0,0 +1,224 @@
+"""Unit tests for the MultisignWalletContract class.
+
+"""
+
+import smartpy as sp
+
+# Import the multisignWalletContract module
+multisignWalletContract = sp.io.import_script_from_url(
+    "file:python/contracts/multisignWalletContract.py")
+
+
+def get_test_environment():
+    # Create the test accounts
+    user1 = sp.test_account("user1")
+    user2 = sp.test_account("user2")
+    user3 = sp.test_account("user3")
+    user4 = sp.test_account("user4")
+
+    # Initialize the multisign wallet contract
+    multisign = multisignWalletContract.MultisignWalletContract(
+        users={user1.address, user2.address, user3.address, user4.address},
+        minimum_votes=3)
+
+    # Add some initial balance to the multisign wallet
+    multisign.set_initial_balance(sp.tez(10))
+
+    # Add the contract to the test scenario
+    scenario = sp.test_scenario()
+    scenario += multisign
+
+    # Save all the variables in a test environment dictionary
+    testEnvironment = {
+        "scenario" : scenario,
+        "user1" : user1,
+        "user2" : user2,
+        "user3" : user3,
+        "user4" : user4,
+        "user1" : user1,
+        "multisign" : multisign}
+
+    return testEnvironment
+
+
+@sp.add_test(name="Test transter tez proposal")
+def test_transfer_tez_proposal():
+    # Get the test environment
+    testEnvironment = get_test_environment()
+    scenario = testEnvironment["scenario"]
+    user1 = testEnvironment["user1"]
+    user2 = testEnvironment["user2"]
+    user3 = testEnvironment["user3"]
+    user4 = testEnvironment["user4"]
+    multisign = testEnvironment["multisign"]
+
+    # Create the account that will receive the tez transfer
+    receptor = sp.test_account("receptor")
+
+    # Add a transfer tez proposal
+    scenario += multisign.transfer_tez_proposal(
+        tez_amount=sp.tez(3),
+        destination=receptor.address).run(sender=user1)
+
+    # Vote for the proposal
+    scenario += multisign.vote_proposal(proposal_id=0, approval=True).run(sender=user1)
+    scenario += multisign.vote_proposal(proposal_id=0, approval=False).run(sender=user2)
+    scenario += multisign.vote_proposal(proposal_id=0, approval=True).run(sender=user3)
+    scenario += multisign.vote_proposal(proposal_id=0, approval=True).run(sender=user4)
+
+    # Execute the proposal
+    scenario += multisign.execute_proposal(0).run(sender=user3)
+
+    # Check that the proposal is listed as executed
+    scenario.verify(multisign.data.proposals[0].executed)
+
+
+@sp.add_test(name="Test transter token proposal")
+def test_transfer_token_proposal():
+    # Get the test environment
+    testEnvironment = get_test_environment()
+    scenario = testEnvironment["scenario"]
+    user1 = testEnvironment["user1"]
+    user2 = testEnvironment["user2"]
+    user3 = testEnvironment["user3"]
+    user4 = testEnvironment["user4"]
+    multisign = testEnvironment["multisign"]
+
+    # Create the dummy token
+    token = sp.test_account("token")
+
+    # Create the account that will receive the tez transfer
+    receptor = sp.test_account("receptor")
+
+    # Add a transfer token proposal
+    scenario += multisign.transfer_token_proposal(
+        token_contract=token.address,
+        token_id=sp.nat(1),
+        token_amount=sp.nat(5),
+        destination=receptor.address).run(sender=user3)
+
+    # Vote for the proposal
+    scenario += multisign.vote_proposal(proposal_id=0, approval=True).run(sender=user1)
+    scenario += multisign.vote_proposal(proposal_id=0, approval=False).run(sender=user2)
+    scenario += multisign.vote_proposal(proposal_id=0, approval=True).run(sender=user3)
+    scenario += multisign.vote_proposal(proposal_id=0, approval=True).run(sender=user4)
+
+    # Execute the proposal
+    scenario += multisign.execute_proposal(0).run(sender=user3)
+
+    # Check that the proposal is listed as executed
+    scenario.verify(multisign.data.proposals[0].executed)
+
+
+@sp.add_test(name="Test minimum votes proposal")
+def test_minimum_votes_proposal():
+    # Get the test environment
+    testEnvironment = get_test_environment()
+    scenario = testEnvironment["scenario"]
+    user1 = testEnvironment["user1"]
+    user2 = testEnvironment["user2"]
+    user3 = testEnvironment["user3"]
+    user4 = testEnvironment["user4"]
+    multisign = testEnvironment["multisign"]
+
+    # Add a minimum votes proposal
+    scenario += multisign.minimum_votes_proposal(2).run(sender=user4)
+
+    # Vote for the proposal
+    scenario += multisign.vote_proposal(proposal_id=0, approval=True).run(sender=user1)
+    scenario += multisign.vote_proposal(proposal_id=0, approval=False).run(sender=user2)
+    scenario += multisign.vote_proposal(proposal_id=0, approval=True).run(sender=user3)
+    scenario += multisign.vote_proposal(proposal_id=0, approval=True).run(sender=user4)
+
+    # Execute the proposal
+    scenario += multisign.execute_proposal(0).run(sender=user3)
+
+    # Check that the minimum votes parameter has been updated
+    scenario.verify(multisign.data.minimum_votes == 2)
+
+
+@sp.add_test(name="Test add user proposal")
+def test_add_user_proposal():
+    # Get the test environment
+    testEnvironment = get_test_environment()
+    scenario = testEnvironment["scenario"]
+    user1 = testEnvironment["user1"]
+    user2 = testEnvironment["user2"]
+    user3 = testEnvironment["user3"]
+    user4 = testEnvironment["user4"]
+    multisign = testEnvironment["multisign"]
+
+    # Create the new user account
+    user5 = sp.test_account("user5")
+
+    # Add a add user proposal
+    scenario += multisign.add_user_proposal(user5.address).run(sender=user4)
+
+    # Vote for the proposal
+    scenario += multisign.vote_proposal(proposal_id=0, approval=True).run(sender=user1)
+    scenario += multisign.vote_proposal(proposal_id=0, approval=False).run(sender=user2)
+    scenario += multisign.vote_proposal(proposal_id=0, approval=True).run(sender=user3)
+    scenario += multisign.vote_proposal(proposal_id=0, approval=True).run(sender=user4)
+
+    # Execute the proposal
+    scenario += multisign.execute_proposal(0).run(sender=user3)
+
+    # Check that the proposal is listed as executed
+    scenario.verify(multisign.data.proposals[0].executed)
+
+    # Check that now there is 5 users
+    scenario.verify(sp.len(multisign.data.users.elements()) == 5)
+    scenario.verify(multisign.data.users.contains(user5.address))
+
+
+@sp.add_test(name="Test remove user proposal")
+def test_remove_user_proposal():
+    # Get the test environment
+    testEnvironment = get_test_environment()
+    scenario = testEnvironment["scenario"]
+    user1 = testEnvironment["user1"]
+    user2 = testEnvironment["user2"]
+    user3 = testEnvironment["user3"]
+    user4 = testEnvironment["user4"]
+    multisign = testEnvironment["multisign"]
+
+    # Add a remove user proposal
+    scenario += multisign.remove_user_proposal(user2.address).run(sender=user4)
+
+    # Vote for the proposal
+    scenario += multisign.vote_proposal(proposal_id=0, approval=True).run(sender=user1)
+    scenario += multisign.vote_proposal(proposal_id=0, approval=False).run(sender=user2)
+    scenario += multisign.vote_proposal(proposal_id=0, approval=True).run(sender=user3)
+    scenario += multisign.vote_proposal(proposal_id=0, approval=True).run(sender=user4)
+
+    # Execute the proposal
+    scenario += multisign.execute_proposal(0).run(sender=user3)
+
+    # Check that the proposal is listed as executed
+    scenario.verify(multisign.data.proposals[0].executed)
+
+    # Check that now there is only 3 users and that the minimum votes were not changed
+    scenario.verify(sp.len(multisign.data.users.elements()) == 3)
+    scenario.verify(~multisign.data.users.contains(user2.address))
+    scenario.verify(multisign.data.minimum_votes == 3)
+
+    # Add another remove user proposal
+    scenario += multisign.remove_user_proposal(user1.address).run(sender=user4)
+
+    # Vote for the proposal
+    scenario += multisign.vote_proposal(proposal_id=1, approval=True).run(sender=user1)
+    scenario += multisign.vote_proposal(proposal_id=1, approval=True).run(sender=user3)
+    scenario += multisign.vote_proposal(proposal_id=1, approval=True).run(sender=user4)
+
+    # Execute the proposal
+    scenario += multisign.execute_proposal(1).run(sender=user3)
+
+    # Check that the proposal is listed as executed
+    scenario.verify(multisign.data.proposals[1].executed)
+
+    # Check that now there is only 2 users
+    scenario.verify(sp.len(multisign.data.users.elements()) == 2)
+    scenario.verify(~multisign.data.users.contains(user1.address))
+
+    # Check that the minimum votes parameter has been updated
+    scenario.verify(multisign.data.minimum_votes == 2)
